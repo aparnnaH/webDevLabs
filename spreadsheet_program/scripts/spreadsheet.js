50448196ca12331ae76d539652c28b94ce7701e3
aparnna-h <98049190+aparnna-h@users.noreply.github.com>
2022-03-27 18:31:52 -0400

    lab06

--- spreadsheet_program/scripts/spreadsheet.js
+++ spreadsheet_program/scripts/spreadsheet.js
@@ -4,7 +4,6 @@ function deselectAll() {
         document.getElementById("headers").style.backgroundColor = '#d0d0d0';
     });
 }
-deselectAll();
 
 function selectRow(){ 
     $(document).ready(function () {
@@ -16,20 +15,85 @@ function selectRow(){
             else {
                 $(this).css('background', 'white');
             }
+            mark = $(tr).find('td').eq(index).text();
+            countGrade(mark);
+            drawChart(count);
         });
     });
 }
-selectRow();
 
 function selectColumn(){ 
     $(document).ready(function () {
         $("#spreadsheet tr th").click(function() {
             $("#spreadsheet td").removeClass("selected");
             var index = $(this).index();
+            var count = [0,0,0,0,0];
+            var mark = 0.0;
             $("#spreadsheet tr").each(function(i, tr) {
                 $(tr).find('td').eq(index).addClass("selected");
+                mark = $(tr).find('td').eq(index).text();
+                countGrade(mark);
             });
+            drawChart(count);
         });
     });
 }
-selectColumn();
+
+function countGrade(mark){
+    switch( getGrade(mark)) {
+        case 'A':
+          count[0] += 1
+          break;
+        case 'B':
+            count[1] += 1
+            break;
+        case 'C':
+            count[2] += 1
+            break;
+        case 'D':
+            count[3] += 1
+            break;
+        case 'F':
+            count[4] += 1
+            break;
+        default:
+            break;
+    }
+}
+
+function drawChart(count){
+    var average = [0.0,0.0,0.0,0.0];
+    var total = count[0] + count[1] + count[2] + count[3] + count[4]
+    for (let i = 0; i < count.length; i++) {
+        average[i] = count[i] / total;
+    }
+    //chart
+    var data = {
+        header: ["Grade", "Frequency (%)"],
+        rows: [
+            ["A", count[0]],
+            ["B", count[1]],
+            ["C", count[2]],
+            ["D", count[3]],
+            ["F", count[4]]
+    ]};
+    var chart = anychart.bar();
+    chart.data(data);
+    chart.title("Grade Distribution");
+    chart.container("container");
+    chart.draw();
+}
+
+function getGrade(mark) {
+    if (mark < 50.0) {
+        return 'F';
+    } else if (mark < 60.0) {
+        return 'D';
+    } else if (mark < 70.0) {
+        return 'C';
+    } else if (mark < 80.0) {
+        return 'B';
+    } else {
+        return 'A';
+    }
+}
